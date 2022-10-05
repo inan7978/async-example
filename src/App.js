bd5ea139a2de7d0148515370104e2e048b64517a
Inan Ismailov <iismailov@espassociates.com>
2022-10-05 15:59:38 -0400

    set up the skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function resolveAfter2Seconds() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("resolved");
+      }, 2000);
+    });
+  }
+
+  async function asyncCall() {
+    let temp = "";
+    console.log("calling");
+    const result = await resolveAfter2Seconds();
+    temp = result;
+    console.log(temp);
+    // expected output: "resolved"
+  }
+
+  asyncCall();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-bar-side">
+        <div className="logo-container nav-bar-containers">
+          <h3>logo</h3>
+        </div>
+        <div className="options-container nav-bar-containers">
+          <h3>options</h3>
+        </div>
+        <div className="bottom-container nav-bar-containers">
+          <h3>bottom</h3>
+        </div>
+      </div>
+      <div className="main-screen">
+        <div className="main-contents">
+          <div className="tester"></div>
+        </div>
+      </div>
     </div>
   );
 }
